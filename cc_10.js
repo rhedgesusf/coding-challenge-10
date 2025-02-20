7d1d1f91d48baeb9e378be70c12165d81b812581
Riley Hedges <rhedges1@usf.edu>
2025-02-20 17:57:26 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -30,3 +30,27 @@ console.log(prod1.getDetails());
 
 prod1.updateStock(3);
 console.log(prod1.getDetails());
+
+/////////////////////////////////////
+// Task 2: Creating an Order Class //
+/////////////////////////////////////
+
+console.log("--------------------------------------");
+console.log("Task 2: Creating an Order Class");
+
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId
+        this.product = product
+        this.quantity = quantity
+        this.product.updateStock(quantity)
+    }
+    getOrderDetails() {
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}`;
+    }
+}
+
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+
+console.log(prod1.getDetails());
